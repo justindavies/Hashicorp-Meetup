2d5304bfd1243da9bdf2cf5acfdd170340025519
Justin Davies <juda@microsoft.com>
2018-05-23 22:40:03 +0100

    test win

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "curl --retry-connrefused --retry --retry-delay 2 -m 60 -f -s http://${azurerm_container_group.aci-iexcompanies.ip_address}"
+    command = "curl --retry-connrefused --retry 30 --retry-delay 2 -m 60 -f -s http://${azurerm_container_group.aci-iexcompanies.ip_address}"
   }
 }
